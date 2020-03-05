441b2250b36b158238850b61c1e9f56b0270234b
chigozieorunta <chigozieorunta@yahoo.com>
2020-03-05 17:28:01 +0100

    Added header, footer and main sections to app container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-import Header from "./components/header";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Main from "./components/Main/Main";
 
 class App extends Component {
   render() {
-    return <Header />;
+    return (
+      <div>
+        <Header></Header>
+        <Main></Main>
+        <Footer></Footer>
+      </div>
+    );
   }
 }
 
