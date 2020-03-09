bcbca0941e2ffa24a2db50b203f0924595b2c867
chigozieorunta <chigozieorunta@yahoo.com>
2020-03-09 23:55:12 +0100

    Added routes

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Projects from "../pages/Projects";
+import Reports from "../pages/Reports";
+import NotFound from "../pages/404";
+
+class Routes extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/reports" exact component={Reports} />
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
