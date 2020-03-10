c4dec06c1d49483f599722ec37fd9a02a90edd20
chigozieorunta <chigozieorunta@yahoo.com>
2020-03-10 08:35:21 +0100

    Added projects template view

---
+++ src/pages/Projects/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Project from "../../components/Projects/Project";
+
+class Projects extends Component {
+  state = {};
+  render() {
+    return <Project />;
+  }
+}
+
+export default Projects;
