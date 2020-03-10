fcd78ae8d86f21db56b81102d92af84088f716d4
chigozieorunta <chigozieorunta@yahoo.com>
2020-03-10 08:35:02 +0100

    Added dashboard template view

---
+++ src/pages/Dashboard/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import Bugs from "../../components/Bugs/Bugs";
+
+class Dashboard extends Component {
+  state = {};
+  render() {
+    return <Bugs />;
+  }
+}
+
+export default Dashboard;
