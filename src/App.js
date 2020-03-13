eac5d78ae90afbdd155f68bdb1d45da03aa794e6
chigozieorunta <chigozieorunta@yahoo.com>
2020-03-13 01:04:32 +0100

    Added app layout js file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,11 @@ import React, { Component } from "react";
 import { Router } from "react-router-dom";
 import Routes from "./routes";
 import History from "./services/history";
-import Header from "./layouts/header";
-import Search from "./layouts/search";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header></Header>
-        <Search></Search>
         <Router history={History}>
           <Routes />
         </Router>
